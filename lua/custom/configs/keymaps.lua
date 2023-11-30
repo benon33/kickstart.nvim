37d8e325dd26969a3e3705eaa441bdd9ade877ba
Ben Mok <benjamin.mok@shipt.com>
2023-11-29 21:47:28 -0800

    new

--- lua/custom/configs/keymaps.lua
+++ lua/custom/configs/keymaps.lua
@@ -30,6 +30,7 @@ keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
 -- Split window
 keymap.set("n", "ss", ":split<Return>", opts)
 keymap.set("n", "sv", ":vsplit<Return>", opts)
+
 -- Move window
 keymap.set("n", "sh", "<C-w>h")
 keymap.set("n", "sk", "<C-w>k")
@@ -37,9 +38,15 @@ keymap.set("n", "sj", "<C-w>j")
 keymap.set("n", "sl", "<C-w>l")
 
 -- Resize window
-keymap.set("n", "<C-w><left>", "<C-w><")
-keymap.set("n", "<C-w><right>", "<C-w>>")
-keymap.set("n", "<C-w><up>", "<C-w>+")
-keymap.set("n", "<C-w><down>", "<C-w>-")
+-- keymap.set("n", "<C-w><left>", "<C-w><")
+-- keymap.set("n", "<C-w><right>", "<C-w>>")
+-- keymap.set("n", "<C-w><up>", "<C-w>+")
+-- keymap.set("n", "<C-w><down>", "<C-w>-")
+--
 
+-- move pane
+keymap.set("n", "<C-0>", "<C-w><C-w>")
+
+-- yank
+keymap.set("v", "y", "+y")
 return {}
